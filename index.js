291c3baae2082ebef21979c6a47820c84d1a71e2
SarnaKhmel <olsarnat@gmail.com>
2020-09-08 11:33:13 +0300

    1th mongodb + express  = index page

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const exprshds = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exprshds.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(todoRoutes)
+
+async function start () {
+  try {
+    mongoose.connect('mongodb+srv://oleksa:qeqelock@cluster0.8ev4f.mongodb.net/todos', {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    })
+    console.log('db connected)')
+    app.listen(PORT, () => {
+      console.log('server has been started...')
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
